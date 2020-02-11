4c7d8779b552b39793e22262b2b254f330a99524
Clemens Schmid <clemens@nevrome.de>
2020-02-11 14:27:34 +0100

    test prep should be skipped on travis

--- tests/testthat/helper-establish_database_connection.R
+++ tests/testthat/helper-establish_database_connection.R
@@ -1,10 +1,15 @@
 # This code runs before the tests.
 
-creds <- readLines(".credentials")
-con <- DBI::dbConnect(
-  RMariaDB::MariaDB(), 
-  host = creds[1],
-  user = creds[2],
-  password = creds[3],
-  db = "pandora"
-)
+# should not run on travis
+if (Sys.getenv("USER") != "travis") {
+
+  creds <- readLines(".credentials")
+  con <- DBI::dbConnect(
+    RMariaDB::MariaDB(), 
+    host = creds[1],
+    user = creds[2],
+    password = creds[3],
+    db = "pandora"
+  )
+
+}
