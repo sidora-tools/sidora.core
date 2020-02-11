dfcd233638ca654a267bf34d3c03951782c65657
Clemens Schmid <clemens@nevrome.de>
2020-02-11 10:26:21 +0100

    simplified test code

--- playground/test.R
+++ playground/test.R
@@ -7,29 +7,8 @@ con <- DBI::dbConnect(
   db = "pandora"
 )
 
-site_con <- get_con("TAB_Site", con)
-site_tibble <- get_df("TAB_Site", con)
-
-
-site_con %>% dplyr::filter(
-  Latitude > 5
-)
-
-site_tibble %>% dplyr::filter(
-  Latitude > 5
-)
-
-filter_by_tag <- function(x, include = c(), exclude = c()) {
-  tags_down <- x %>% dplyr::select("Tags") %>% dplyr::collect() %>% dplyr::pull("Tags")
-  include_all <- unname(sapply(tags_down, function(y) {all(sapply(include, function(z) {grepl(z, y)}))}))
-  exlude_any <- unname(sapply(tags_down, function(y) {any(sapply(exclude, function(z) {grepl(z, y)}))}))
-  filter_condition <- include_all & !exlude_any
-  filter_id <- which(filter_condition)
-  x %>% dplyr::filter(Id %in% filter_id)
-}
-
-filter_by_tag(site_con, include = c("DFG_Spain_KWA_legacy", "Wolfgang Haak"), exclude =  c("James Fellows Yates"))
-filter_by_tag(site_tibble, include = c("DFG_Spain_KWA_legacy", "Wolfgang Haak"), exclude =  c("James Fellows Yates"))
+get_con("TAB_Site", con)
+get_df("TAB_Site", con)
 
 #### get multiple connections ####
 
@@ -43,13 +22,22 @@ df_list <- get_df_list(c(
   "TAB_Site", "TAB_Individual", "TAB_Sample", "TAB_Extract", "TAB_Library"
 ), con = con)
 
-df_list <- get_df_list(c(
-  "TAB_Site", "TAB_Individual"
-), con = con)
-
 jt <- join_pandora_tables(df_list)
 
 get_df_list(c(
   "TAB_Site", "TAB_Individual"
 ), con = con) %>% join_pandora_tables()
 
+#### experimental: filter by tag ####
+
+filter_by_tag <- function(x, include = c(), exclude = c()) {
+  tags_down <- x %>% dplyr::select("Tags") %>% dplyr::collect() %>% dplyr::pull("Tags")
+  include_all <- unname(sapply(tags_down, function(y) {all(sapply(include, function(z) {grepl(z, y)}))}))
+  exlude_any <- unname(sapply(tags_down, function(y) {any(sapply(exclude, function(z) {grepl(z, y)}))}))
+  filter_condition <- include_all & !exlude_any
+  filter_id <- which(filter_condition)
+  x %>% dplyr::filter(Id %in% filter_id)
+}
+
+filter_by_tag(site_con, include = c("DFG_Spain_KWA_legacy", "Wolfgang Haak"), exclude =  c("James Fellows Yates"))
+filter_by_tag(site_tibble, include = c("DFG_Spain_KWA_legacy", "Wolfgang Haak"), exclude =  c("James Fellows Yates"))
