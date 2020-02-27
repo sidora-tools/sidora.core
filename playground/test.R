ca1addd736fde00939068658ef4e8dc037fead0f
Clemens Schmid <clemens@nevrome.de>
2020-02-27 22:46:27 +0100

    some more comments in test script

--- playground/test.R
+++ playground/test.R
@@ -1,5 +1,7 @@
 library(magrittr)
 
+#### establish connection to the Pandora database server ####
+
 creds <- readLines("playground/.credentials")
 con <- DBI::dbConnect(
   RMariaDB::MariaDB(), 
@@ -9,20 +11,26 @@ con <- DBI::dbConnect(
   db = "pandora"
 )
 
+#### get tables ####
+
+# as DBI connection
 get_con("TAB_Site", con)
+# as a local data.frame (data is cached in a tempdir by default)
 get_df("TAB_Site", con)
 
-#### get multiple connections ####
+#### get multiple tables ####
 
+# as list of DBI connections
 get_con_list(c(
   "TAB_Site", "TAB_Individual", "TAB_Sample", "TAB_Extract", "TAB_Library"
 ), con = con)
 
-#### joining ####
-
+# as list of data.frames
 df_list <- get_df_list(c(
   "TAB_Site", "TAB_Individual", "TAB_Sample", "TAB_Extract", "TAB_Library"
 ), con = con)
+
+#### join tables ####
 
 jt <- join_pandora_tables(df_list)
 
