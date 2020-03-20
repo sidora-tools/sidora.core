79032a7942a735622e2d6e8d143b3f336bd72c1b
Clemens Schmid <clemens@nevrome.de>
2020-03-20 16:37:23 +0100

    I think I found a nice solution for the filter function. Please take a look, @jfy133

---
+++ R/filter_data.R
@@ -0,0 +1,29 @@
+#' Filter a merged PANDORA table by project or tag
+#'
+#' @param x data.frame
+#' @param col character. String column in x (e.g. "Projects.Individual" or "Tags.Individual") on which
+#' the filter process should be applied
+#' @param ins character vector. Projects or tags you want to see 
+#' @param outs character vector. Projects of tags you do not want to see
+#'
+#' @return filtered data.frame
+#' 
+#' @export
+filter_pr_tag <- function(x, col, ins = c(), outs = c()) {
+
+  if (!(length(ins) == 0) & (length(outs) == 0)) {
+    dplyr::filter(x, is_in(ins, .data[[col]]))
+  } else if ((length(ins) == 0) & !(length(outs) == 0)) {
+    dplyr::filter(x, !is_in(outs, .data[[col]]))
+  } else if (!(length(ins) == 0) & !(length(outs) == 0)) {
+    dplyr::filter(x, is_in(ins, .data[[col]]) & !is_in(outs, .data[[col]]))
+  } else {
+    x
+  }
+  
+}
+
+is_in <- function (ss, vec) {
+  list_of_grep_result_vectors <- lapply(ss, function(s) { grepl(s, vec) })
+  purrr::reduce(list_of_grep_result_vectors, `|`)
+}
