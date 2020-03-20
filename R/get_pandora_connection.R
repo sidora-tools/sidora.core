bf11aacb9d3f1ebe4b7f269de3fe3602cbeaed19
Clemens Schmid <clemens@nevrome.de>
2020-03-20 15:38:14 +0100

    function to establish database connection to pandora with a .credentials file

---
+++ R/get_pandora_connection.R
@@ -0,0 +1,29 @@
+#' Get the connection object for the Pandora DB 
+#' 
+#' @param cred_file A credentials file containing three lines
+#' listing host, user and password, respectively
+#'
+#' @return The Pandora DB connection object.
+#' 
+#' @export
+get_pandora_connection <- function (cred_file = ".credentials") {
+  
+  if (!file.exists(cred_file)) {
+    stop(paste(
+      "Can't find .credentials file. Please create one ",
+      "containing three lines:", 
+      "the database host, the username, the password."
+    ))
+  }
+  
+  creds <- readLines(cred_file)
+  con <- DBI::dbConnect(
+    RMariaDB::MariaDB(), 
+    host = creds[1],
+    user = creds[2],
+    password = creds[3],
+    db = "pandora"
+  )
+  
+  return(con)
+}
