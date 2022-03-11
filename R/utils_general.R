9ca29f550d52b07c9e22bb597c54e9f042d8da99
James Fellows Yates <jfy133@gmail.com>
2022-03-11 12:57:47 +0100

    Add little helper function for printing pandora docs

--- R/utils_general.R
+++ R/utils_general.R
@@ -23,3 +23,44 @@ check_if_packages_are_available <- function(packages_ch) {
     )
   }
 }
+
+#' Access field Pandora help tooltip
+#'
+#' This functions allows you to call the 'tooltip' help/documentation message
+#' present on the Pandora webpage for a given entry.
+#'
+#' @param tab character. Name of the table of the field of interest
+#' @param field character vector. Name of the field of interest
+#' @param con database connection object
+#'
+#' @return help text of the given field
+#' 
+#' @name field_help
+#' 
+#' @export
+
+field_help <- function(tab, field, con) {
+  
+  if ( !tab %in% sidora.core::pandora_tables ) {
+    stop(crayon::red("[sidora.core] error: "), "Select one valid PANDORA SQL table.")
+    
+  }
+  
+  if (!field %in% sidora.core::pandora_column_types$col_name ) {
+    stop(crayon::red("[sidora.core] error: "), "Field name is not recognised.")
+  }
+  
+  pandora_help <- sidora.core::get_df("TAB_Field_Comment", con) %>% tibble::as_tibble()
+  result <- dplyr::filter(pandora_help, .data$Table == tab, .data$Field == field) %>% 
+    dplyr::pull(.data$Comment) 
+  
+  if ( length(result) < 1 ) { result = "Currently no help message for this field is available. Please complain on ~Pandora." }
+    
+    paste0(crayon::bold("Pandora Help Message\n"), 
+           crayon::blue(tab), ": ", 
+           crayon::green(field), 
+           "\n", 
+           result) %>% 
+    cat
+}
+  
