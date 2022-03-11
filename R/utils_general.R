36c2ab4eb69150d7f7fa57a05d8d971e6225166e
James Fellows Yates <jfy133@gmail.com>
2022-03-11 15:57:37 +0100

    Changes after review, add personal note how to not be stupid for devs.

--- R/utils_general.R
+++ R/utils_general.R
@@ -29,8 +29,10 @@ check_if_packages_are_available <- function(packages_ch) {
 #' This functions allows you to call the 'tooltip' help/documentation message
 #' present on the Pandora webpage for a given entry.
 #'
-#' @param tab character. Name of the table of the field of interest
-#' @param field character vector. Name of the field of interest
+#' @param tab character. Name of the table of the field of interest in
+#'           "TAB_<table>" format. E.g. "TAB_Sample"
+#' @param field character vector. Name of the field of interest. In Pandora
+#'              format E.g. "Sampled_Quantity"
 #' @param con database connection object
 #'
 #' @return help text of the given field
@@ -42,23 +44,25 @@ check_if_packages_are_available <- function(packages_ch) {
 field_help <- function(tab, field, con) {
   
   if ( !tab %in% sidora.core::pandora_tables ) {
-    stop(crayon::red("[sidora.core] error: "), "Select one valid PANDORA SQL table.")
+    stop("[sidora.core] error: Select one valid PANDORA SQL table.")
     
   }
   
   if (!field %in% sidora.core::pandora_column_types$col_name ) {
-    stop(crayon::red("[sidora.core] error: "), "Field name is not recognised.")
+    stop("[sidora.core] error: Pandora field name is not recognised.")
   }
   
-  pandora_help <- sidora.core::get_df("TAB_Field_Comment", con) %>% tibble::as_tibble()
-  result <- dplyr::filter(pandora_help, .data$Table == tab, .data$Field == field) %>% 
+  pandora_help <- sidora.core::get_df("TAB_Field_Comment", con)
+  colnames(pandora_help) <- sidora_col_name_to_col_name(colnames(pandora_help))
+  result <- pandora_help %>% 
+    dplyr::filter(., .data$Table == tab & .data$Field == field) %>% 
     dplyr::pull(.data$Comment) 
   
   if ( length(result) < 1 ) { result = "Currently no help message for this field is available. Please complain on ~Pandora." }
     
-    paste0(crayon::bold("Pandora Help Message\n"), 
-           crayon::blue(tab), ": ", 
-           crayon::green(field), 
+    paste0("PANDORA HELP MESSAGE\n", 
+           tab, ": ", 
+           field, 
            "\n", 
            result) %>% 
     cat
