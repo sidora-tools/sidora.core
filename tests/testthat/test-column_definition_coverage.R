1c20ff9a28306a414826e206f8ed7de56b53f202
Clemens Schmid <clemens@nevrome.de>
2023-04-25 16:58:46 +0200

    added a test to check if we have all columns in pandora covered

---
+++ tests/testthat/test-column_definition_coverage.R
@@ -0,0 +1,44 @@
+context("pandora_column_types.tsv")
+
+test_that("all columns in Pandora tables are defined in sidora.core", {
+  
+  skip_on_ci()
+  
+  # get all tables and their columns in Pandora
+  cons_to_all_tables <- get_con_list(pandora_tables, con)
+  
+  pandora_columns <- cons_to_all_tables %>%
+    purrr::map2_dfr(names(.), ., function(pandora_table_name, pandora_table_con) {
+      tibble::tibble(table = pandora_table_name, col_name = colnames(pandora_table_con))
+    }) %>%
+    dplyr::arrange(table, col_name) %>%
+    dplyr::mutate(
+      column = paste0(table, "::", col_name)
+    ) %>% dplyr::pull("column")
+  
+  # get all tables and columns sidora.core knows
+  sidora_columns <- pandora_column_types %>%
+    dplyr::select(table, col_name) %>%
+    dplyr::arrange(table, col_name) %>%
+    dplyr::mutate(
+      column = paste0(table, "::", col_name)
+    ) %>% dplyr::pull("column")
+  
+  # compare column lists
+  in_pandora_not_sidora <- setdiff(pandora_columns, sidora_columns)
+  in_sidora_not_pandora <- setdiff(sidora_columns, pandora_columns)
+  
+  if (length(c(in_pandora_not_sidora, in_sidora_not_pandora)) != 0) {
+    warning(
+      "Columns in Pandora, but not pandora_column_types.tsv:\n",
+      paste(in_pandora_not_sidora, collapse = ", "),
+      "\n",
+      "Columns in pandora_column_types.tsv, but not Pandora:\n",
+      paste(in_sidora_not_pandora, collapse = ", ")
+    )
+  }
+  
+  # check if the pandora and the sidora column selection is equal
+  expect_identical(pandora_columns, sidora_columns)
+  
+})
